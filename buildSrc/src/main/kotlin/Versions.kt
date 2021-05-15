0098e4c6a61ae7b78ea6bdb1b75671a7d7e81403
Shreyas Muthkur <shreyasspidy@gmail.com>
2021-05-15 15:38:40 -0500

    Latest Studio 4.2.1 compatibility fix

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
 
-    const val gradle = "4.1.3"
+    const val gradle = "4.2.1"
     const val kotlin = "1.4.21"
     const val kotlinCoroutine = "1.4.0"
     const val coRoutineAdapter = "0.9.2"
@@ -32,8 +32,8 @@ object Versions {
     const val googleTruth = "1.1.2"
     const val mockk = "1.10.0"
 
-    const val robolectric = "4.3"
-    const val robolectricShadows = "4.3"
+    const val robolectric = "4.5"
+    const val robolectricShadows = "4.5"
     const val kotlinCoroutineTest = "1.4.2"
     const val nhaarmanMockito = "2.2.0"
     const val okHttpMockServer = "4.7.2"
